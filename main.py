aa191ead7f9bc455e4908ca45f3789213a0a84a7
Nicole <124882220+nikkichep@users.noreply.github.com>
2024-05-31 08:50:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+ # complete game of rock, paper, scissors
+player1 = 'rock'
+player2 = 'scissors'
+
+if player1 == player2:
+   print('Its a tie')
+
+
+
+if player1 == 'rock':
+    if player2 == 'paper':
+     print('player 2 won')
+    elif player2 == 'scissors':
+       print('player 1 won')
+
+if player1 == 'paper':
+    if player2 == 'rock':
+     print('player 1 won')
+    elif player2 == 'scissors':
+     print('player 2 won')
+if player1 == 'scissors':
+    if player2 == 'rock':
+     print('player 2 won')
+    elif player2 == 'paper':
+     print('player 1 won')
